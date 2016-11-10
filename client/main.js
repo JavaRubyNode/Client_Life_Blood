a0c6e9dc62d5df50b4207818eb4708b6a24237d7
JavaRubyNode <viniciusjavaruby@gmail.com>
2016-11-10 09:58:29 -0200

    modificacao no main.js

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
+
 import './main.html';
 import '../imports/ui/doador.js';
 import '../imports/ui/html/comboSangue.html';
@@ -9,9 +10,24 @@ import '../imports/ui/html/navBar.html';
 import '../imports/ui/html/logoDoador.html';
 import '../imports/ui/html/cadastroDoador.html';
 import '../imports/ui/html/tabelaDoador.html';
+import '../imports/ui/accountsConfig.js';
 
 
 
 Template.registerHelper('isLogado', function() {
     return Meteor.userId();
 });
+
+
+Meteor.startup(() => {
+    T9n.setLanguage('pt');
+});
+
+Template.body.events({
+    'click .js-sair'(e) {
+        e.preventDefault();
+        AccountsTemplates.logout();
+    }
+});
+
+
